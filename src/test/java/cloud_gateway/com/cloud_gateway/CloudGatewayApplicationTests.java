a044d824461a5da3723dd83e561bc24d3d935c53
MBI-88 <ingmbi8807@gmail.com>
2025-06-16 08:17:08 -0300

    layout done

---
+++ src/test/java/cloud_gateway/com/cloud_gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud_gateway.com.cloud_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
