b4fb7885b68ade4da9898798d127b583f26d1b4d
MBI-88 <ingmbi8807@gmail.com>
2025-06-24 07:51:37 -0300

    cloud gateway done

--- src/main/java/cloud_gateway/com/cloud_gateway/CloudGatewayApplication.java
+++ src/main/java/cloud_gateway/com/cloud_gateway/CloudGatewayApplication.java
@@ -2,11 +2,15 @@ package cloud_gateway.com.cloud_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
